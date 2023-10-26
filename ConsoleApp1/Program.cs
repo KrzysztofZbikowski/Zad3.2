649e32ebfe54c4b768e7596793c797bf051397b6
Zbiku <Zbiku@MSI>
2023-10-26 22:43:10 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+        Console.WriteLine("Podaj a: ");
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj b: ");
+        b = int.Parse(Console.ReadLine());
+        if(a%b==0)
+        {
+            Console.WriteLine("liczba {0} jest dzielnikiem liczby {1}", b, a);
+        }
+        else
+        {
+            Console.WriteLine("liczba {0} nie jest dzielnikiem liczby {1}", b, a);
+        }
+        Console.ReadKey();
+    }
+}
